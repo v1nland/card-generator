d3bbe409b9d5f59c62c4bfd5df073750cfdcf2f5
Valentina Díaz Gonzalez <valentina@MacBook-Air-de-Valentina.local>
2020-05-21 23:41:26 -0400

    Final tarjeta

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import gato from '../imagenes/gato3.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as FontAwesome from '@fortawesome/free-solid-svg-icons';
 import { exportComponentAsJPEG, exportComponentAsPDF, exportComponentAsPNG } from 'react-component-export-image';
@@ -7,14 +8,15 @@ class ComponentToPrint extends React.Component {
     render(){
         return (
                 <div className="blog-card">
-                  <h1>Papeleria.Gatobacam</h1>
-                  <div className="datos-usario">
-                    <p>Nombre: Valentina almendra gonzalez gonzalez</p>
-                    <p>Rut: 19994721-4 </p>
-                    <p>Celular: 950731812  </p>
-                    <p>Dirección:    </p>
+                  <img className="logo" src={gato} alt="gato" />
+                  <h1>Papeleria.Gatobacam </h1>
+                  <div className="datos-usuario">
+                    <p>Nombre: jgjgjgjhghjgjhghjghjghjgjh </p>
+                    <p>Rut: 666666666666  </p>
+                    <p>Celular:888888888  </p>
+                    <p>Dirección:mmmmmmmm    </p>
                   </div>
-                    
+
                   <h2><u><center>Muchas gracias por tu compra</center></u></h2>
 
                   <div className="datos-bbr">

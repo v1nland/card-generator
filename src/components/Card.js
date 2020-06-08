304e1b9f28a41efe9939f68e24c9f438f923c01a
Martín Saavedra Núñez <martin.saavedra@mail.udp.cl>
2020-06-08 15:31:13 -0400

    Subrayado de número de pedido y arreglo de municipio de destino.

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,69 +11,63 @@ class Card extends Component{
     }
 
     render(){
-        if ( this.props.nombreUsuario == "" || this.props.nombreUsuario == null ) {
+        if ( this.props.nombreUsuario === "" || this.props.nombreUsuario === null ) {
             return null;
         } else {
             return (
                 <div className="blog-card">
-                  <img className="logo" src={gato} alt="gato" />
-                  <h1>Papeleria.Gatobacam </h1>
+                    <img className="logo" src={gato} alt="gato" />
+                    <h1>Papeleria.Gatobacam </h1> <br />
 
-                  <br />
+                    <div className="datos-usuario">
+                        <table className="tablita">
+                            <tr>
+                                <td className="fijacion2">Nombre:</td>
+                                <td className="fijacion">{this.props.nombreUsuario}</td>
+                                <td></td>
+                            </tr>
 
-                  <div className="datos-usuario">
-                    <table className="tablita">
+                            <tr>
+                                <td>RUT:</td>
+                                <td>{this.props.rutUsuario} </td>
+                                <td></td>
+                            </tr>
 
-                      <tr>
-                            <td className="fijacion2">Nombre:</td>
-                            <td className="fijacion">{this.props.nombreUsuario}</td>
-                            <td></td>
-                      </tr>
+                            <tr>
+                                <td>Celular:</td>
+                                <td>{this.props.celularUsuario} </td>
+                                { this.props.tipoPedido !== 'Envíos por Starken a domicilio (modalidad por pagar)'?
+                                    <td className="overlay">Nº Pedido: {this.props.numeroPedido}</td>
+                                    :
+                                    <td>Nº Pedido: {this.props.numeroPedido}</td>
+                                }
+                            </tr>
 
+                            <tr>
+                                <td>Dirección:</td>
+                                <td>{this.props.direccionUsuario}</td>
+                                <td></td>
+                            </tr>
 
-                      <tr>
-                            <td>RUT:</td>
-                            <td>{this.props.rutUsuario} </td>
-                            <td></td>
-                          </tr>
+                            <tr>
+                                <td> </td>
+                                <td>{this.props.comunaUsuario} </td>
+                                <td></td>
+                            </tr>
+                        </table>
+                    </div>
 
+                    <br />
 
-                      <tr>
-                            <td>Celular:</td>
-                            <td>{this.props.celularUsuario} </td>
-                            <td>NºPedido: {this.props.Npedido}</td>
-                          </tr>
+                    <h2><u><center>Muchas gracias por tu compra</center></u></h2>
 
+                    <br />
 
-                       <tr>
-                            <td>Dirección:</td>
-                            <td>{this.props.direccionUsuario}</td>
-                            <td></td>
-
-                          </tr>
-
-
-                       <tr>
-                            <td> </td>
-                            <td>{this.props.comunaUsuario} </td>
-                            <td></td>
-                          </tr>
-
-                    </table>
-                  </div>
-
-                  <br />
-
-
-                  <h2><u><center>Muchas gracias por tu compra</center></u></h2>
-
-                  <br />
-
-                  <div className="datos-bbr">
-                    <p>Remitente: Bárbara Inayado Araya</p>
-                    <p>Rut: 19.513.795-1</p>
-                    <p>Celular: 930777736</p>
-                  </div>
+                    <div className="datos-bbr">
+                        <p>Remitente: Bárbara Inayado Araya</p>
+                        <p>Rut: 19.513.795-1</p>
+                        <p>Celular: 930777736</p>
+                    </div>
                 </div>
             );
         }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,21 +24,22 @@ class Home extends Component{
         this.setState({ csv: data }, () => {
             for (var i = 0; i < this.state.csv.length; i = i+2) {
                 var newElem = [];
-                newElem.push(this.state.csv[i][12])
-                newElem.push(this.state.csv[i][36])
-                newElem.push(this.state.csv[i][37])
-                newElem.push(this.state.csv[i][13])
-                newElem.push(this.state.csv[i][30])
-                newElem.push(this.state.csv[i][0])
+                newElem.push(this.state.csv[i][12]) // Letra M: Nombre envio
+                newElem.push(this.state.csv[i][36]) // Letra AK: RUT
+                newElem.push(this.state.csv[i][37]) // Letra AL: Telefono
+                newElem.push(this.state.csv[i][13]) // Letra N: Direccion de envio
+                newElem.push(this.state.csv[i][19]) // Letra T: Municipio de facturacion
+                newElem.push(this.state.csv[i][0]) // Letra A: Numero de pedido
+                newElem.push(this.state.csv[i][20]) // Letra U: Tipo de envío
 
                 if (i+1 < this.state.csv.length) {
-                    newElem.push(this.state.csv[i+1][12])
-                    newElem.push(this.state.csv[i+1][36])
-                    newElem.push(this.state.csv[i+1][37])
-                    newElem.push(this.state.csv[i+1][13])
-                    newElem.push(this.state.csv[i+1][30])
-                    newElem.push(this.state.csv[i+1][0])
-
+                    newElem.push(this.state.csv[i+1][12]) // Letra M: Nombre envio
+                    newElem.push(this.state.csv[i+1][36]) // Letra AK: RUT
+                    newElem.push(this.state.csv[i+1][37]) // Letra AL: Telefono
+                    newElem.push(this.state.csv[i+1][13]) // Letra N: Direccion de envio
+                    newElem.push(this.state.csv[i+1][19]) // Letra T: Municipio de facturacion
+                    newElem.push(this.state.csv[i+1][0]) // Letra A: Numero de pedido
+                    newElem.push(this.state.csv[i+1][20]) // Letra U: Tipo de envío
                 }
 
                 this.state.newCsv.push(newElem);
@@ -56,18 +57,20 @@ class Home extends Component{
                     celularUsuario={csvData[2]}
                     direccionUsuario={csvData[3]}
                     comunaUsuario={csvData[4]}
-                    Npedido={csvData[5]}
+                    numeroPedido={csvData[5]}
+                    tipoPedido={csvData[6]}
                 />
             </Col>
 
             <Col>
                 <Card
-                    nombreUsuario={csvData[6]}
-                    rutUsuario={FormatRUT(csvData[7])}
-                    celularUsuario={csvData[8]}
-                    direccionUsuario={csvData[9]}
-                    comunaUsuario={csvData[10]}
-                    Npedido={csvData[11]}
+                    nombreUsuario={csvData[7]}
+                    rutUsuario={FormatRUT(csvData[8])}
+                    celularUsuario={csvData[9]}
+                    direccionUsuario={csvData[10]}
+                    comunaUsuario={csvData[11]}
+                    numeroPedido={csvData[12]}
+                    tipoPedido={csvData[13]}
                 />
             </Col>
         </Row>
@@ -86,9 +89,12 @@ class Home extends Component{
                         <p>2. Espera a que se generen las tarjetas</p>
                         <p>3. Presiona Ctrl + P (en Mac, Command + P)</p>
                         <p>4. ¡Guarda el PDF listo para su impresión!</p>
+
                         <hr />
+
                         <p>De preferencia, utilizar Google Chrome.</p>
                         <p>El tiempo de generación de tarjetas depende de las características de tu computadora y del tamaño del archivo.</p>
+
                         <Form.Group as={Col} style={{textAlign: 'center'}}>
                             <Form.Label className="file-input" htmlFor="csv-button">{ file }</Form.Label>
                             <CSVReader
@@ -107,24 +113,4 @@ class Home extends Component{
         );
     }
 }
-
-// class Home extends Component{
-//     constructor(props, context){
-//         super(props, context);
-//
-//         this.componentRef = React.createRef();
-//     }
-//
-//     render(){
-//         return(
-//             <React.Fragment>
-//                 <button onClick={() => exportComponentAsPDF(this.componentRef)}>
-//                     Descargar como PDF
-//                 </button>
-//
-//                 <ComponentToPrint ref={this.componentRef} />
-//             </React.Fragment>
-//         );
-//     }
-// }
 export default Home;
